7e90dd830db97a4d902e1b15ce6ce6f24b5e972c
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-10 14:10:34 -0400

    used a single state function to add input to an object and sent to an api

--- src/App.js
+++ src/App.js
@@ -2,37 +2,28 @@ import "./App.css";
 import { useState } from "react";
 import { useEffect } from "react";
 function App() {
-  const [formSubmitted,setFormSubmitted] = useState(false)
-  const [validForm,setValidForm]=useState(false)
-  const [errorMessage,setErrorMessage]=useState('')
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [validForm, setValidForm] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [form, setForm] = useState({});
+
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("This is the description");
-  const [author, setAuthor] = useState("Todd");
   // console.log(title);
 
-  useEffect(()=>{
+  useEffect(() => {
     //fetch stuff
-    if (title && description){
-      setValidForm(true)
+    if (form?.title?.length>3 && form?.description?.length>10) {
+      setValidForm(true);
     }
-  },[title,description,author])
-
-
-
-
-
-
-
-
+  }, [form]);
 
   async function formSubmit(e) {
     e.preventDefault();
-   if (!validForm){
-    setErrorMessage("Not a valid Form")
-    return
-   }
-   
-   
+    if (!validForm) {
+      setErrorMessage("Not a valid Form");
+      return;
+    }
 
     console.log("form submitted");
 
@@ -41,68 +32,75 @@ function App() {
     //   description:description,
     //   author:author
     // }
-    const comment = {
-      title,
-      description,
-      author,
-    };
+    
 
-    console.log(comment);
-try{
-    const results = await fetch("https://sql.bocacode.com/comments", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(comment),
-    });
-    console.log(results);
-    const data = await results.json();
-    console.log(data);
-    setFormSubmitted(true)
-    setErrorMessage()
-    // alert()
-  }catch (error){
-    console.log(error)
-    setErrorMessage("There was an error submitting your comment" + error.toString())
-  }
-  }
+    ;
+    try {
+      const results = await fetch("https://sql.bocacode.com/comments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+      console.log(results);
+      const data = await results.json();
+      console.log(data);
+      setFormSubmitted(true);
+      setErrorMessage();
+      // alert()
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(
+        "There was an error submitting your comment" + error.toString()
+      );
+    }
+  }{console.log(form)}
+
+const updateForm = (event) =>{
+ // setForm({[event.target.name]: event.target.value})
+ setForm({...form,[event.target.name]: event.target.value})
+}
+
+
   return (
     <div className="App">
       <form onSubmit={formSubmit}>
         <h1>Comments</h1>
+
         {/* here goes the title */}
         <label>Title</label>
         <input
-        //required
+          name="title"
+          //required
           type="text"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
+          //value={form}
+          onChange={updateForm}
+          
         />
         <h3>{title}</h3>
         {/* This is the description */}
         <label>Description</label>
-        <textarea required
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+        <textarea
+          required
+          name='description'
+          value={form.description}
+          onChange={updateForm}
         ></textarea>
-        <h3>{description}</h3>
+        <h3>{form.description}</h3>
 
         <label>Author</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+        <select name='Author' value={form.Author} onChange={updateForm}>
           <option value="">Choose One</option>
           <option value="Todd">Todd</option>
           <option value="Lud">Ludwigson</option>
           <option value="Matt">Matt</option>
           <option value="Other">Other</option>
         </select>
-        <h3>{author}</h3>
+        <h3>{form.Author}</h3>
 
-       {!formSubmitted&& <button>Submit form</button>}
-       {errorMessage && <h1>There was an error: {errorMessage}</h1>}
-        
+        {!formSubmitted && <button >Submit form</button>}
+        {errorMessage && <h1>There was an error: {errorMessage}</h1>}
       </form>
     </div>
   );
